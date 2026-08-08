b28603e73adcf0c5eeba207855c8147d43ab6a96
mohammad <mohammadbehiiii76@gmail.com>
2026-08-08 14:30:34 +0330

    add seeder
    در مرحله آخر namespace  ها ادیت میشه

---
+++ Modules/Course/database/seeders/courseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB ;
+
+class courseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('courses')->insert([
+
+                'title' => 'laravel' ,
+                'image' => '1.jpg' ,
+                'price' => 2000 ,
+                'teacher_id' => 1
+
+        ]) ;  
+
+    }
+}
---
+++ Modules/Course/database/seeders/user_courseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB ;
+
+class user_courseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('user_courses')->insert([
+
+            'user_id' => 1 ,
+            'course_id' => 1
+
+
+        ]);
+    }
+}
