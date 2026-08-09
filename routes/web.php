eceb6106f998eea913aae84ec1c09ec4f3b4606f
mohammad <mohammadbehiiii76@gmail.com>
2026-08-09 09:55:12 +0330

    delete

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController ;
 use App\Http\Controllers\UserController ;
 
-Route::get('/users' , [UserController::class , 'getUser'])->name('getUser');
+// Route::get('/users' , [UserController::class , 'getUser'])->name('getUser');
 
